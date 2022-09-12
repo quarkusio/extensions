0db6cba8c66d4b5b98ca8e3b8593a6045c6d5370
Holly Cummins <hcummins@redhat.com>
2022-09-12 22:48:20 +0100

    Tidy deprecation warning

--- jest.config.js
+++ jest.config.js
@@ -22,7 +22,7 @@ module.exports = {
   globals: {
     __PATH_PREFIX__: ``,
   },
-  testURL: `http://localhost`,
+  testEnvironmentOptions: { url: `http://localhost` },
   setupFiles: [`<rootDir>/loadershim.js`],
   setupFilesAfterEnv: ["<rootDir>/jest-setup.js"],
   testEnvironment: `jsdom`,
