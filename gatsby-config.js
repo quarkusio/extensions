009ec4a907c4d2b9ef96c4158237d4fbc79ba937
Holly Cummins <hcummins@redhat.com>
2022-10-28 11:32:27 +0100

    Apply branding to manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,15 +95,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Quarkus Extensions`,
+        short_name: `Extensions`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/profile-pic.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-fontawesome-css`,
