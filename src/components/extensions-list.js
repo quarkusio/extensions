95db08e082094f12b5389cf8aea722122d5cf457
Holly Cummins <hcummins@redhat.com>
2022-11-11 11:33:13 +0000

    Refactor filtering logic to separate function

--- src/components/extensions-list.js
+++ src/components/extensions-list.js
@@ -21,6 +21,21 @@ const Extensions = styled.ol`
   width: 100%;
 `
 
+const filterExtensions = (extensions, { categoryFilter, regex }) => {
+  return extensions
+    .filter(extension =>
+      extension.name.toLowerCase().match(regex.toLowerCase())
+    )
+    .filter(
+      extension =>
+        categoryFilter.length === 0 ||
+        (extension.metadata.categories &&
+          extension.metadata.categories.find(category =>
+            categoryFilter.includes(category.toLowerCase())
+          ))
+    )
+}
+
 const ExtensionsList = ({ extensions }) => {
   const [regex, setRegex] = useState(".*")
   const [categoryFilter, setCategoryFilter] = useState([])
@@ -39,6 +54,9 @@ const ExtensionsList = ({ extensions }) => {
       ...new Set(extensions.map(extension => extension.origins).flat()),
     ]
 
+    const filters = { regex, categoryFilter }
+    const filteredExtensions = filterExtensions(extensions, filters)
+
     return (
       <FilterableList className="extensions-list">
         <Filters
@@ -47,25 +65,13 @@ const ExtensionsList = ({ extensions }) => {
           filterActions={filterActions}
         />
         <Extensions>
-          {extensions
-            .filter(extension =>
-              extension.name.toLowerCase().match(regex.toLowerCase())
+          {filteredExtensions.map(extension => {
+            return (
+              <li key={extension.name}>
+                <ExtensionCard extension={extension} />
+              </li>
             )
-            .filter(
-              extension =>
-                categoryFilter.length === 0 ||
-                (extension.metadata.categories &&
-                  extension.metadata.categories.find(category =>
-                    categoryFilter.includes(category.toLowerCase())
-                  ))
-            )
-            .map(extension => {
-              return (
-                <li key={extension.name}>
-                  <ExtensionCard extension={extension} />
-                </li>
-              )
-            })}
+          })}
         </Extensions>
       </FilterableList>
     )
