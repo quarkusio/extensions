646d591bb3551880fb50e8e59403157b51094a95
Holly Cummins <hcummins@redhat.com>
2022-11-22 18:39:05 +0000

    Fix missing null check logic exposed by tests delivered after initial PR

--- src/components/util/extension-slugger.js
+++ src/components/util/extension-slugger.js
@@ -2,17 +2,19 @@ const parse = require("mvn-artifact-name-parser").default
 const slugify = require("slugify")
 
 const extensionSlug = gav => {
-  let string
-  if (gav && gav.includes(":")) {
-    const coordinates = parse(gav)
-    string = coordinates.groupId + "_" + coordinates.artifactId
-  } else {
-    string = gav
+  if (gav) {
+    let string
+    if (gav.includes(":")) {
+      const coordinates = parse(gav)
+      string = coordinates.groupId + "_" + coordinates.artifactId
+    } else {
+      string = gav
+    }
+    // We don't want dots in the url even though they are technically allowed
+    const deDotted = string.replace(/\./g, "-")
+    return slugify(deDotted, {
+      lower: true,
+    })
   }
-  // We don't want dots in the url even though they are technically allowed
-  const deDotted = string.replace(/\./g, "-")
-  return slugify(deDotted, {
-    lower: true,
-  })
 }
 module.exports = { extensionSlug }
