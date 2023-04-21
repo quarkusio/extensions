75706a1d7d63ef07ee16c7b4e3365ff4c9b355e4
Holly Cummins <hcummins@redhat.com>
2023-04-21 16:17:26 +0100

    Nudge some font sizes down

--- src/components/filters/title.js
+++ src/components/filters/title.js
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 const Title = styled.label`
   width: 224px;
-  font-size: var(--font-size-22);
+  font-size: var(--font-size-18);
   letter-spacing: 0;
   color: var(--grey-2);
 `
