7ac0bc3ea2b7028522fc3ed79c509be79d9c9661
Holly Cummins <hcummins@redhat.com>
2023-06-08 17:49:48 +0100

    Add missing test, and refactor to simplify fallback sequences

--- src/components/util/guide-url-rewriter.test.js
+++ src/components/util/guide-url-rewriter.test.js
@@ -45,6 +45,31 @@ describe("the guide url rewriter", () => {
       )
     })
 
+    describe("when a link is well-formed but no guide exists at any version", () => {
+      const badLink = "https://quarkus.io/guides/nonsense"
+
+      beforeEach(() => {
+        urlExist.mockImplementation(() => Promise.resolve(false))
+      })
+
+      afterEach(() => {
+        jest.clearAllMocks()
+      })
+
+      it("does not map the dead link to anything else", async () => {
+        expect(
+          await rewriteGuideUrl({
+            name: "kind-of-dropped-extension",
+
+            metadata: {
+              guide: badLink,
+              maven: { version: "2.16.0" },
+            },
+          })
+        ).toBe(badLink)
+      })
+    })
+
     it("strips nonexistent links in deprecated extensions", async () => {
       expect(
         await rewriteGuideUrl({
@@ -80,88 +105,92 @@ describe("the guide url rewriter", () => {
         ).toBe(validLink)
       })
     })
-  })
 
-  describe("and a valid link exists for an older version", () => {
-    describe("in quarkus guides format", () => {
-      const validLink = "https://quarkus.io/version/2.13/guides/kogito-dmn"
-      const badLink = "https://quarkus.io/guides/kogito-dmn"
-
-      beforeEach(() => {
-        urlExist.mockImplementation(url => Promise.resolve(url === validLink))
-      })
-
-      afterEach(() => {
-        jest.clearAllMocks()
-      })
-
-      it("maps dead links to live links at the older version", async () => {
-        expect(
-          await rewriteGuideUrl({
-            name: "kind-of-dropped-extension",
-
-            metadata: {
-              guide: badLink,
-              maven: { version: "2.16.0" },
-            },
-          })
-        ).toBe(validLink)
-      })
-    })
-  })
-
-  describe("in camel format", () => {
     describe("and a valid link exists for an older version", () => {
-      const validLink =
-        "https://camel.apache.org/camel-quarkus/2.16.x/reference/extensions/sortof-lapsed.html"
-      const badLink =
-        "https://camel.apache.org/camel-quarkus/latest/reference/extensions/sortof-lapsed.html"
+      describe("in quarkus guides format", () => {
+        const validLink = "https://quarkus.io/version/2.13/guides/kogito-dmn"
+        const badLink = "https://quarkus.io/guides/kogito-dmn"
 
-      beforeEach(() => {
-        urlExist.mockImplementation(url => Promise.resolve(url === validLink))
+        beforeEach(() => {
+          urlExist.mockImplementation(url => Promise.resolve(url === validLink))
+        })
+
+        afterEach(() => {
+          jest.clearAllMocks()
+        })
+
+        it("maps dead links to live links at the older version", async () => {
+          expect(
+            await rewriteGuideUrl({
+              name: "kind-of-dropped-extension",
+
+              metadata: {
+                guide: badLink,
+                maven: { version: "2.16.0" },
+              },
+            })
+          ).toBe(validLink)
+        })
       })
 
-      afterEach(() => {
-        jest.clearAllMocks()
-      })
+      describe("in camel format", () => {
+        describe("and a valid link exists for an older version", () => {
+          const validLink =
+            "https://camel.apache.org/camel-quarkus/2.16.x/reference/extensions/sortof-lapsed.html"
+          const badLink =
+            "https://camel.apache.org/camel-quarkus/latest/reference/extensions/sortof-lapsed.html"
 
-      it("maps dead links to live links at the older version", async () => {
-        expect(
-          await rewriteGuideUrl({
-            name: "kind-of-dropped-extension",
-
-            metadata: {
-              guide: badLink,
-              maven: { version: "2.16.0" },
-            },
+          beforeEach(() => {
+            urlExist.mockImplementation(url =>
+              Promise.resolve(url === validLink)
+            )
           })
-        ).toBe(validLink)
-      })
-    })
 
-    describe("and a valid link exists for a snapshot version", () => {
-      const validLink =
-        "https://camel.apache.org/camel-quarkus/next/reference/extensions/hot-off-the-press.html"
-      const badLink =
-        "https://camel.apache.org/camel-quarkus/latest/reference/extensions/hot-off-the-press.html"
-
-      beforeEach(() => {
-        urlExist.mockImplementation(url => Promise.resolve(url === validLink))
-      })
-
-      afterEach(() => {
-        jest.clearAllMocks()
-      })
-
-      it("maps dead links to live snapshot links", async () => {
-        expect(
-          await rewriteGuideUrl({
-            name: "hot-off-the-press-extension",
-            metadata: {
-              guide: badLink,
-            },
+          afterEach(() => {
+            jest.clearAllMocks()
           })
-        ).toBe(validLink)
+
+          it("maps dead links to live links at the older version", async () => {
+            expect(
+              await rewriteGuideUrl({
+                name: "kind-of-dropped-extension",
+
+                metadata: {
+                  guide: badLink,
+                  maven: { version: "2.16.0" },
+                },
+              })
+            ).toBe(validLink)
+          })
+        })
+
+        describe("and a valid link exists for a snapshot version", () => {
+          const validLink =
+            "https://camel.apache.org/camel-quarkus/next/reference/extensions/hot-off-the-press.html"
+          const badLink =
+            "https://camel.apache.org/camel-quarkus/latest/reference/extensions/hot-off-the-press.html"
+
+          beforeEach(() => {
+            urlExist.mockImplementation(url =>
+              Promise.resolve(url === validLink)
+            )
+          })
+
+          afterEach(() => {
+            jest.clearAllMocks()
+          })
+
+          it("maps dead links to live snapshot links", async () => {
+            expect(
+              await rewriteGuideUrl({
+                name: "hot-off-the-press-extension",
+                metadata: {
+                  guide: badLink,
+                },
+              })
+            ).toBe(validLink)
+          })
+        })
       })
     })
   })
