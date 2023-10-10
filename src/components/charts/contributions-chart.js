89e90929a1b6e6716c738361b6e195d076c875c2
Holly Cummins <hcummins@redhat.com>
2023-10-10 21:31:15 +0100

    Use black text on pie chart for accessibility, add text list of contributors when we can't fit them on the pie chart

--- src/components/charts/contributions-chart.js
+++ src/components/charts/contributions-chart.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { getPalette } from "../util/styles/style"
-import { Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts"
+import { LabelList, Legend, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts"
 import PropTypes from "prop-types"
 
 
@@ -13,14 +13,38 @@ const ContributionsChart = (props) => {
     const contributors = uncolouredContributors.sort((a, b) => b.contributions - a.contributions).map((contributor, i) => {
       return { ...contributor, fill: palette[i] }
     })
-    
+
+    const renderLegendText = (value, entry) => {
+      const color = "black"
+
+      const commitCount = entry?.payload?.value
+      const commits = commitCount > 1 ? `${commitCount} commits` : commitCount === 1 ? `${commitCount} commit` : ""
+
+      return <span><span style={{ color, display: "inline-block", width: "180px" }}>{value}</span><span
+        style={{
+          color, width: "100px",
+          display: "inline-block",
+          "text-align": "right"
+        }}>{commits}</span></span>
+    }
+
+    const lotsOfContributors = contributors.length > 8
+
+    //  we set a blank label if there are a small number of contributors, so we get the line, but we define our own
+    // text so we can make it black. the offset in the label list is hand-tuned to put the text near the end of the line
     return (
       <ResponsiveContainer width={700} height="80%">
         <PieChart title={"Committers"}
                   desc={`A pie chart showing that ${contributors[0].name} has made the most commits in the past six months.`}>
-          <Pie data={contributors} dataKey="contributions" nameKey="name"
-               label={contributors.length < 8 ? (contributor) => contributor.name : false}>
+          <Pie data={contributors} dataKey="contributions" nameKey="name" innerRadius={80}
+               label={lotsOfContributors ? false : () => ""}
+          >
+            {lotsOfContributors ||
+              <LabelList dataKey="name" position="outside" offset={21} stroke="none" fill="black" />}
           </Pie>
+          {lotsOfContributors && <Legend layout="vertical" align="right" verticalAlign="top" iconType={"circle"}
+                                         style={{ color: "black" }} formatter={renderLegendText} />}
+
           <Tooltip />
 
         </PieChart>
