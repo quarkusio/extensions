c469953db09c47ba63bb6dc9c0f536a7f4513774
Holly Cummins <hcummins@redhat.com>
2024-07-03 10:45:41 +0100

    Bump GitHub API

--- site-validation/bad-image-issue.java
+++ site-validation/bad-image-issue.java
@@ -2,7 +2,7 @@
 
 //JAVA 17+
 
-//DEPS org.kohsuke:github-api:1.322
+//DEPS org.kohsuke:github-api:1.323
 //DEPS info.picocli:picocli:4.2.0
 
 import com.fasterxml.jackson.core.JsonProcessingException;
