a3fb4d0146899caa78b0eeb4fb01e24f9b47a146
Holly Cummins <hcummins@redhat.com>
2024-07-03 12:21:14 +0100

    Use forked GitHub API to properly handle 429s

--- site-validation/dead-link-issue.java
+++ site-validation/dead-link-issue.java
@@ -18,7 +18,7 @@
 
 //JAVA 17+
 
-//DEPS org.kohsuke:github-api:1.323
+//DEPS https://github.com/holly-cummins/github-api/commit/0ba8970874b97b8ee7c024cba5a63a73cc480bb9
 //DEPS info.picocli:picocli:4.2.0
 
 import com.fasterxml.jackson.core.JsonProcessingException;
