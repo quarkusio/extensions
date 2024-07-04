f7b2ef4eaedfa288e40a45d8e2ae843336549182
Holly Cummins <hcummins@redhat.com>
2024-07-04 16:12:17 +0100

    Do not use broken snapshot reference

--- site-validation/dead-link-issue.java
+++ site-validation/dead-link-issue.java
@@ -18,7 +18,7 @@
 
 //JAVA 17+
 
-//DEPS https://github.com/holly-cummins/github-api/tree/main
+//DEPS https://github.com/holly-cummins/github-api/tree/main#:SNAPSHOT
 //DEPS info.picocli:picocli:4.2.0
 
 import com.fasterxml.jackson.core.JsonProcessingException;
