0cbe9fff725f04133bda33eca63897d26ba2e49b
Holly Cummins <hcummins@redhat.com>
2024-07-04 20:33:16 +0100

    Allow css for svg fontawesome to be inlined

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,3 +11,9 @@ import "@fontsource/open-sans/300.css"
 import "@fontsource/open-sans/400.css"
 import "@fontsource/open-sans/600.css"
 import "@fontsource/open-sans/700.css"
+
+// This ensures that the icon CSS is loaded immediately before attempting to render icons
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
+// Prevent fontawesome from dynamically adding its css since we did it manually above
+config.autoAddCss = false
