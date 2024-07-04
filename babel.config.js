363ba83f48e359a36ed772d45344f64454c46c61
Holly Cummins <hcummins@redhat.com>
2024-07-04 16:44:04 +0100

    Do not use babel loose mode, to resolve warning

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   "plugins": [
-    ["@babel/plugin-transform-private-methods", { "loose": true }],
-    ["@babel/plugin-transform-private-property-in-object", { "loose": true }]
+    ["@babel/plugin-transform-private-methods", { loose: "false" }],
+    ["@babel/plugin-transform-private-property-in-object", { loose: "false" }],
+    ["@babel/plugin-transform-class-properties", { loose: "false" }],
   ],
   "presets": [
     [
