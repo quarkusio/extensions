a50e9a3f40e530a4aff32600f745c77fe2e0b802
Holly Cummins <hcummins@redhat.com>
2025-01-21 21:15:41 +0000

    Add a witty(ish) 404 page for bad dates on the new-extensions route.

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,10 +8,23 @@ import { initialiseDisplayModeFromLocalStorage } from "../components/util/dark-m
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
 
+  const isLookingForNewExtensions = location.pathname && location.pathname.includes("/new-extensions/")
+  const isPast = location.pathname && location.pathname.includes("/new-extensions/201")
+  const isFuture = location.pathname && location.pathname.includes("/new-extensions/202")
+
+  let message
+  if (isLookingForNewExtensions) {
+    const date = location.pathname.replaceAll("/new-extensions/", "").split("/").reverse().join(" ")
+    const explanation = isPast ? `No Quarkus extensions were released in ${date}.` : isFuture ? `The Quarkus extensions for ${date} haven't been created yet.` : `There are no Quarkus extensions for ${date}.`
+    message = "Quarkus contributors can do a lot of nifty things, but time travel isn't one of them. " + explanation
+  } else {
+    message = "You just hit a route that doesn&#39;t exist... the sadness."
+  }
+
   return (
     <Layout location={location} title={siteTitle}>
       <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>{message}</p>
     </Layout>
   )
 }
